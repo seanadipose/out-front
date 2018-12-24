38fddd6aa0307b3c43aa8410b9e74f188e80702d
sean <hamiltonsean6@gmail.com>
2018-12-24 14:32:36 -0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+
+const settings = environment;
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(settings)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAawQRrihOjrV3vr-MDOg9pLHoU41zifls",
+    authDomain: "crusader-e7684.firebaseapp.com",
+    databaseURL: "https://crusader-e7684.firebaseio.com",
+    projectId: "crusader-e7684",
+    storageBucket: "crusader-e7684.appspot.com",
+    messagingSenderId: "373357936061"
+  }
 };
 
 /*
